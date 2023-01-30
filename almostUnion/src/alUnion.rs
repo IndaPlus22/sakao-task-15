972b9b725bc8296d515ea08c1ddd59ea99c0980e
toshi <toshi.sakao.r@gmail.com>
2023-01-31 03:15:20 +0900

    i dont know why this doesnt work

--- almostUnion/src/alUnion.rs
+++ almostUnion/src/alUnion.rs
@@ -1,23 +1,114 @@
-
-
-pub struct al_union {
-    sets: Vec<Vec<usize>>,
-    locations: Vec<usize>,
-    size: usize
+#[derive(Debug)]
+struct Element {
+    value: usize,
+    parent: usize,
 }
 
-impl al_union {
-    pub fn new(size: usize) -> al_union {
-        let mut sets: Vec<Vec<usize>> = Vec::with_capacity(size);
-        let locations: Vec<usize> = (0..size).map(|v| v).collect();
+impl Element {
+    fn new(val: usize, par: usize) -> Element {
+        Element {
+            value: val,
+            parent: par,
+        }
+    }
+}
+
+pub struct BetUnion {
+    values: Vec<Element>,
+    // locations: Vec<usize>
+}
+
+impl BetUnion {
+    pub fn new(size: usize) -> BetUnion {
+        let mut values: Vec<Element> = Vec::new();
+        // let locations: Vec<usize> = (0..size).map(|v| v).collect();
         for i in 0..size {
-            sets[i] = vec![i; 1];
+            values.push(Element::new(i + 1, i));
         }
-        al_union {
-            sets,
-            locations,
-            size
+
+        BetUnion {
+            values, /*locations */
         }
+    }
+
+    // union the sets that contain num1 and num2
+    pub fn union(&mut self, num1: usize, num2: usize) {
+        let old_parent = self.find(num1);
+        let new_parent = self.find(num2);
+        if old_parent == new_parent {
+            return;
+        }
+        let tmp: Vec<bool> = self.values.iter().map(|x| x.parent == old_parent).collect();
+
+        // println!("debug:::union: {:?}", tmp);
+        for i in 0..tmp.len() {
+            if tmp[i] {
+                self.values[i].parent = new_parent;
+                // self.locations[i] = new_parent  ;
+            }
+        }
+
+        // println!("res: {:?}", self.values);
+        // println!("loc: {:?}", self.locations);
+    }
+
+    // move num1 to set containing num2
+    pub fn move_val(&mut self, num1: usize, num2: usize) {
+        let new_parent = self.find(num2);
+        if new_parent == self.find(num1) {
+            return;
+        }
+        self.values[num1 - 1].parent = new_parent;
+        // println!("new parent {}, num1: {}", new_parent, num1);
+        // self.locations[num1   - 1] = new_parent  ;
+
+        // println!("res: {:?}", self.values);
+        // println!("loc: {:?}", self.locations);
+    }
+
+    // returns (size of set containing num, sum of numbers in set containing num)
+    pub fn get(&self, num: usize) -> (usize, usize) {
+        let parent = self.find(num);
+        let tmp: Vec<bool> = self.values.iter().map(|x| x.parent == parent).collect();
+        let mut val: usize = 0;
+        let mut sum: usize = 0;
+
+        // println!("debug:::get: {:?}", tmp);
+        for i in 0..tmp.len() {
+            if tmp[i] {
+                sum += i + 1;
+                val += 1;
+            }
+        }
+
+        (val, sum)
+    }
+
+    // returns which set number is in e.g. number 3 is in set number 2(sets are indexed from 0)
+    fn find(&self, num: usize) -> usize {
+        self.values[num - 1].parent
+    }
+}
+
+
+// old version
+pub struct AlUnion {
+    sets: Vec<Vec<usize>>,
+    locations: Vec<usize>,
+}
+
+impl AlUnion {
+    pub fn new(size: usize) -> AlUnion {
+        let mut sets: Vec<Vec<usize>> = Vec::new();
+        for i in 0..size {
+            sets.push(Vec::new());
+        }
+        let locations: Vec<usize> = (0..size).map(|v| v).collect();
+
+        for i in 0..size {
+            sets[i] = vec![i + 1; 1];
+        }
+        AlUnion { sets, locations }
     }
 
     // union the sets that contain num1 and num2
@@ -33,10 +124,13 @@ impl al_union {
         tmp.append(&mut self.sets[set1]);
 
         self.sets[set1] = tmp;
-        self.locations[num2 -1] = set1;
+        self.locations[(num2 - 1)] = set1;
+
+        // println!("res: {:?}", self.sets);
+        // println!("loc: {:?}", self.locations);
     }
 
-    // move num2 to set containing num1
+    // move num1 to set containing num2
     pub fn move_val(&mut self, num1: usize, num2: usize) {
         let set1 = self.find(num1);
         let set2 = self.find(num2);
@@ -44,11 +138,14 @@ impl al_union {
             return;
         }
 
-        let rem_index = self.sets[set2].iter().position(|&x| x == num2).unwrap();
+        let rem_index = self.sets[set1].iter().position(|&x| x == num1).unwrap();
 
-        self.sets[set1].push(num2);
-        self.sets[set2].remove(rem_index);
-        self.locations[num2 - 1] = set1;
+        self.sets[set2].push(num1);
+        self.sets[set1].remove(rem_index);
+        self.locations[(num1 - 1)] = set2;
+
+        // println!("res: {:?}", self.sets);
+        // println!("loc: {:?}", self.locations);
     }
 
     // returns (size of set containing num, sum of numbers in set containing num)
@@ -60,6 +157,6 @@ impl al_union {
 
     // returns which set num is in e.g. number 3 is in set number 2(sets are indexed from 0)
     fn find(&self, num: usize) -> usize {
-        self.locations[num - 1]
+        self.locations[(num - 1)]
     }
 }
--- almostUnion/src/main.rs
+++ almostUnion/src/main.rs
@@ -1,12 +1,11 @@
 mod alUnion;
 
+use alUnion::*;
 use std::io::{self, stdin};
-use alUnion::al_union;
-
 
 fn main() {
     let mut input = String::new();
-    stdin().read_line(&mut input);
+    stdin().read_line(&mut input).unwrap();
     let mut nm: Vec<usize> = input
         .trim()
         .split(" ")
@@ -14,23 +13,26 @@ fn main() {
         .collect();
 
     input.clear();
-    let mut union_thing: al_union = al_union::new(nm[0]);
+    // let mut union_thing: al_union = al_union::new(nm[0]);
+    let mut union_thing: BetUnion = BetUnion::new(nm[0]);
     for i in 0..nm[1] {
-        stdin().read_line(&mut input);
+        stdin().read_line(&mut input).unwrap();
         let mut cmd: Vec<usize> = input
             .trim()
             .split(" ")
             .map(|x| x.parse().expect("Could not parse numbers!"))
             .collect();
-        
+
         match cmd[0] {
             1 => union_thing.union(cmd[1], cmd[2]),
             2 => union_thing.move_val(cmd[1], cmd[2]),
             3 => {
                 let (size, sum) = union_thing.get(cmd[1]);
-                println!("{}, {}", size, sum);
-            },
-            _ => ()
+                println!("{} {}", size, sum);
+            }
+            _ => (),
         }
+
+        input.clear();
     }
 }
