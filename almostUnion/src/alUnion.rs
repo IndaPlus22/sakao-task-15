3fdd385c893ed14a80e8accc3a9aa7beb4fdee00
toshi <toshi.sakao.r@gmail.com>
2023-01-31 12:01:11 +0100

    hdd

--- almostUnion/src/alUnion.rs
+++ almostUnion/src/alUnion.rs
@@ -1,6 +1,6 @@
 use std::ops::*;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 struct Element {
     value: usize,
     parent: usize,
@@ -49,14 +49,24 @@ impl BetUnion {
             return;
         }
         let tmp: Vec<bool> = self.values.iter().map(|x| x.parent == old_parent).collect();
-
-        // println!("debug:::union: {:?}", tmp);
+        
         for i in 0..tmp.len() {
             if tmp[i] {
                 self.values[i].parent = new_parent;
             }
         }
 
+        // -------thought this would be faster but wasnt----------------------
+        // let mut tmp1: Vec<Element> = self.values.clone();
+        // tmp1 = tmp1.drain(..).filter(|x| x.parent == old_parent).collect();
+
+        // for i in 0..tmp1.len() {
+        //     self.values[tmp1[i].value - 1].parent = new_parent;
+        // }
+        // -----------------------------
+
+        // println!("debug:::union: {:?}", tmp);
+        // println!("tmp1: {:?}", tmp1);
         // println!("res: {:?}", self.values);
     }
 
@@ -92,6 +102,76 @@ impl BetUnion {
     // returns which set number is in e.g. number 3 is in set number 2(sets are indexed from 0)
     fn find(&self, num: usize) -> usize {
         self.values[num - 1].parent
+    }
+}
+
+// ver 3
+pub struct ThUnion {
+    rank: Vec<usize>,
+    parent: Vec<usize>,
+    size: usize
+}
+
+impl ThUnion {
+    pub fn new(size: usize) -> ThUnion {
+        let rank: Vec<usize> = vec![0; size];
+        let parent: Vec<usize> = (0..size).map(|v| v).collect();
+
+        ThUnion { rank, parent, size }
+    }
+
+    // union the sets that contain num1 and num2
+    pub fn union(&mut self, num1: usize, num2: usize) {
+        let set1 = self.find(num1);
+        let set2 = self.find(num2);
+        if set1 == set2 {
+            return;
+        }
+
+        if self.rank[set1] > self.rank[set2] {
+            self.parent[set1] = set2;
+        } else if self.rank[set1] < self.rank[set2] {
+            self.parent[set2] = set1;
+        } else {
+            self.parent[set2] = set1;
+            self.rank[set1] += 1;
+        }
+        
+        println!("parent: {:?}", self.parent);
+        println!("rank: {:?}", self.rank);
+    }
+
+    // move num1 to set containing num2
+    pub fn move_val(&mut self, num1: usize, num2: usize) {
+        let set1 = self.find(num1);
+        let set2 = self.find(num2);
+        if set1 == set2 {
+            println!("skipped");
+            return;
+        }
+
+        self.parent[num1 - 1] = set2;
+        self.rank[set2] += 1;
+
+        println!("parent: {:?}", self.parent);
+        println!("rank: {:?}", self.rank);
+    }
+
+    // returns (size of set containing num, sum of numbers in set containing num)
+    pub fn get(&mut self, num: usize) -> (usize, usize) {
+        let parent = self.find(num);
+        
+        (1, 1)
+        // (self.sets[set].len(), self.sets[set].iter().sum())
+    }
+
+    // returns set of num by finding the root.
+    fn find(&mut self, num: usize) -> usize {
+        if self.parent[num -1] != num -1 {
+            self.parent[num -1] = self.find(num -1);
+        }
+
+        self.parent[num -1]
     }
 }
 
